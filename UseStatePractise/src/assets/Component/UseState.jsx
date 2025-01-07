2aeff482d8038894063e591a363182734a59242e
Maheboob7699 <murdimaheboob@gmail.com>
2025-01-07 01:10:06 -0900

    useStatePractise

--- UseStatePractise/src/assets/Component/UseState.jsx
+++ UseStatePractise/src/assets/Component/UseState.jsx
@@ -1,16 +1,13 @@
 
 import { useState } from "react"
 function UseStatePractise() {
-    const [name, setName] = useState("Mehboob");
     const[toogle, setToogle] = useState(false);
 
     function changeName() {
         setToogle(!toogle)
-    setName(toogle ? "Anuj" : "Mehboob")
     }
     return (
-        <>
-        <h1> Hello{name}</h1>
+        <> <h1>{toogle ? "anuj" : "Mehboob"}</h1><br />
             <button onClick={changeName}>Click Me</button>
         </>
     )
