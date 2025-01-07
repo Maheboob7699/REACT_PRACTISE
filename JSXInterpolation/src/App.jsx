f83b6bfe56fe744a7cad53ba401f87cbf96311e6
Maheboob7699 <murdimaheboob@gmail.com>
2025-01-07 00:18:13 -0900

    JSX Practise

---
+++ JSXInterpolation/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Jsx from './assets/Component/JSX';
+
+function App() {
+let name = "Murdi Mehboob";
+
+  return (
+    <>
+  <Jsx/>
+    </>
+  )
+}
+
+export default App
---
+++ JSXInterpolation/src/assets/Component/JSX.jsx
@@ -0,0 +1,10 @@
+function Jsx() {
+    let name = "Murdi Mehboob";
+    return (
+        <>
+            <h1>Hello {name}</h1>
+        </>
+    )
+}
+
+export default Jsx
