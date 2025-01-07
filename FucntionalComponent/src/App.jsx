f03bacad7e7d22b4db3a2f9abb200bb5abdeb0d6
Maheboob7699 <murdimaheboob@gmail.com>
2025-01-07 00:37:26 -0900

    FucntionalComponent

---
+++ FucntionalComponent/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import FunctionalComponent from './assets/Component/Functional'
+
+function App() {
+
+
+  return (
+    <>
+     <FunctionalComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ FucntionalComponent/src/assets/Component/Functional.jsx
@@ -0,0 +1,8 @@
+function FunctionalComponent(){
+  return(
+    <>
+     <h1>Hello MEhboob Welocme tO React JS</h1>
+    </>
+  )
+}
+export default FunctionalComponent
