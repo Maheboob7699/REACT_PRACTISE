14300b45680e85f357ab403c320a53629803666d
Maheboob7699 <murdimaheboob@gmail.com>
2025-01-07 00:36:27 -0900

    ClassComponent

---
+++ ClassComponent/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import ClassComponent from './assets/Component/ClassBAse'
+
+function App() {
+
+  return (
+    <>
+     <ClassComponent/>
+    </>
+  )
+}
+
+export default App
---
+++ ClassComponent/src/assets/Component/ClassBase.jsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+class ClassComponent extends Component{
+    render(){
+        return(
+            <>
+            <h1>Hello Component</h1>
+            </>
+        )
+    }
+}
+export default ClassComponent
