9d2f9f4272f9f728efc0b8864a3c7b0096f513a5
Maheboob7699 <murdimaheboob.com>
2025-01-15 14:43:05 +0530

    changes in state

--- ClassComponentState/src/Componeent/State.jsx
+++ ClassComponentState/src/Componeent/State.jsx
@@ -1,45 +1,45 @@
 
-import React, {Component} from "react";
-  class StateComponent extends Component{
-    constructor() {
-        super();
-        this.state = {
-          count: 0,
-          name: "Mehboob"
-        };
+import React, { Component } from "react";
+class StateComponent extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+      name: "Mehboob"
+    };
 
 
-      }
+  }
 
-      increment = () => {
-        this.setState({ count: this.state.count + 1 });
-      };
-      decrement =()=>{
-        if(this.state.count > 0){
-            this.setState({count:this.state.count-1})
-        }
-       
-      }
-      
-      changeName=()=>{
-       this.setState({name: "musharaf"})
-      }
+  increment = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  decrement = () => {
+    if (this.state.count > 0) {
+      this.setState({ count: this.state.count - 1 })
+    }
 
-   render(){
-    return(
-        <> 
+  }
+
+  changeName = () => {
+    this.setState({ name: "musharaf" })
+  }
+
+  render() {
+    return (
+      <>
         <h1>Component</h1>
         <h1>Count :  {this.state.count}</h1>
         <h1>Name: {this.state.name}</h1>
         <button onClick={this.increment}>Incerement</button>
         <button onClick={this.decrement}>Decrement</button>
         <button onClick={this.changeName}>Change</button>
-        </>
+      </>
     )
-   }
   }
+}
 
-  export default StateComponent
+export default StateComponent
 
 
 
