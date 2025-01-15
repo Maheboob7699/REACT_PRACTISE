dc4879f7057878be986b8afd0e26ab79aa28e5bd
Maheboob7699 <murdimaheboob.com>
2025-01-15 14:14:13 +0530

    remove unnecessary space

---
+++ ClassBaseProps/src/Component/ClassProps.jsx
@@ -0,0 +1,14 @@
+
+import React,{Component} from "react"
+
+class ClassProps extends Component{
+    render(){
+        return (
+            <>
+             <h1>Class Props</h1>
+            </>
+        )
+    }
+}
+
+export default ClassProps
