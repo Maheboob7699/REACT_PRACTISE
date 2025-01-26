ca332d81d1ea219b972460e7cf674ce5f0894c4b
Maheboob7699 <murdimaheboob@gmail.com>
2025-01-26 05:22:12 -0900

    display the data passing context component

---
+++ ContextApi/src/Component/Display.jsx
@@ -0,0 +1,25 @@
+
+import { useContext } from "react"
+import { todoData } from "./Context"
+function DisplayData(){
+
+    const {input,handleDelete} = useContext(todoData);
+    
+    return(
+
+
+        <>
+          {
+            input.store.map((item,index)=>{
+              return(
+                <div key={index}>
+                 <li>{item.todoValue} </li>
+                 <button onClick={()=>handleDelete(index)}>delete</button>
+                </div>
+              )
+            })
+          }
+        </>
+    )
+}
+export default DisplayData
