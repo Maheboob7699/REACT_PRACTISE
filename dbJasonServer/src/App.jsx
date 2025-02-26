f8527f7075fb5474425a05fec4e423a487911338
Maheboob7699 <murdimaheboob.com>
2025-02-26 08:51:31 +0530

    use Jason server

---
+++ dbJasonServer/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import DbJasonServer from './containers/DbJasonServer'
+
+function App() {
+
+  return (
+    <>
+      <DbJasonServer/>
+    </>
+  )
+}
+
+export default App
